1daa7d774ff08cb46d9a74eed3af4bb347e2e8e3
Eugene <evgen29031999@bk.ru>
2022-03-27 18:31:19 +0300

    add TextField and Label

--- UserDefaults/ViewController.swift
+++ UserDefaults/ViewController.swift
@@ -7,26 +7,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var field: UITextField!
+    @IBOutlet weak var label: UILabel!
+    
+    let userDefaults = UserDefaults()
+    
     //UserDefaults предназначен для хранения пользовательских настроек
-    var userDefaults = UserDefaults.standard
+    //var userDefaults = UserDefaults.standard
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let bool = true
+        field.delegate = self
         
-        userDefaults.set(bool, forKey: "someBool")
+        guard let value = userDefaults.value(forKey: "name") as? String else {
+            return
+        }
+        
+        label.text = value
+        
+//        let bool = true
+//        userDefaults.set(bool, forKey: "someBool")
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
-        let myBool = userDefaults.bool(forKey: "someBool")
-        print(myBool)
-        
+//        let myBool = userDefaults.bool(forKey: "someBool")
+//        print(myBool)
     }
 
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        userDefaults.setValue(field.text, forKey: "name")
+        field.resignFirstResponder()
+        return true
+    }
 
 }
 
